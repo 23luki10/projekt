873f69f13cb13b3165d5c21105c41ac1cedc426e
uczen <23luki10@wp.pl>
2017-03-13 17:21:04 +0100

    Githiub

---
+++ app/src/main/java/com/lukasz_burchacki/projekt/projekt.java
@@ -0,0 +1,13 @@
+package com.lukasz_burchacki.projekt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class projekt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projekt);
+    }
+}
